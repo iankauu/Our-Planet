3ec16863ee8ee920c146bf53af46e67585eb447f
andyjohn23 <andrewnick906@gmail.com>
2023-03-06 16:00:21 +0300

    configure project structure

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,6 @@ namespace App\Http\Controllers;
 
 class PagesController extends Controller {
     public function getHome() {
-      
+      return view('pages.home');
     }
 }
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <h2>Header page</h2>
-</div>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+    <div class="container-fluid" style="margin: 0; padding: 0;">
+        <a class="navbar-brand" href="/">Our Planet</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto main-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Explore</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Facts</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
