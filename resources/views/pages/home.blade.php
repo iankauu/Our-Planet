4cd0b5c9158225d59932451991f239fc9ba0a34a
andyjohn23 <andrewnick906@gmail.com>
2023-03-06 15:21:38 +0300

    create pagescontroller and includes, layouts, pages views and landing page route

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="96x96" href="/static/images/icon.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Our Planet</title>
+</head>
+<body>
+    <h2>Revolution Andy {{ $data['fullname'] }}</h2>
+    <h3>My email is {{ $data['email'] }}</h3>
+</body>
+</html>
