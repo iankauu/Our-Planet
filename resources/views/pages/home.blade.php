0f3c115056f0eb3d911300f7dfac0909be85b5c7
andyjohn23 <andrewnick906@gmail.com>
2023-03-07 07:09:33 +0300

    rectify video width issue

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.base')
 @section('content')
 <div class="home__container container-fluid">
-    <video autoplay loop muted plays-inline class="climate-video">
-        <source src="{{ url('images/climate-change.m4v') }}" type="video/mp4">
-    </video>
-    <div class="effect__container container-fluid">
+    <video src="{{ url('images/climate-change.m4v') }}" autoplay loop muted></video>
+    <div class="effect__container">
         <h1>Understanding our planet to benefit humankind</h1>
         <div class="row effect__elements">
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4 id="effect_element1">Carbon Dioxide</h4>
+                        <h4 id="effect_element3">Carbon Dioxide</h4>
                         <i class="fa fa-arrow-up" aria-hidden="true"></i>
                         <span class="total__number">419</span>
                     </div>
@@ -19,9 +17,36 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4 id="effect_element2">Ocean Warming</h4>
+                        <h4 id="effect_element3">Carbon Dioxide</h4>
                         <i class="fa fa-arrow-up" aria-hidden="true"></i>
-                        <span class="total__number">345</span>
+                        <span class="total__number">419</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 id="effect_element3">Carbon Dioxide</h4>
+                        <i class="fa fa-arrow-up" aria-hidden="true"></i>
+                        <span class="total__number">419</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 id="effect_element3">Carbon Dioxide</h4>
+                        <i class="fa fa-arrow-up" aria-hidden="true"></i>
+                        <span class="total__number">419</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 id="effect_element3">Carbon Dioxide</h4>
+                        <i class="fa fa-arrow-up" aria-hidden="true"></i>
+                        <span class="total__number">419</span>
                     </div>
                 </div>
             </div>
