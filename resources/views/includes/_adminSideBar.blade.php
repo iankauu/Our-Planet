659e0ed366a27ec0c639e0501d34252e63966ee9
andyjohn23 <andrewnick906@gmail.com>
2023-03-11 20:38:28 +0300

    add relevant data to admin sidebar

--- resources/views/includes/_adminSideBar.blade.php
+++ resources/views/includes/_adminSideBar.blade.php
@@ -6,52 +6,72 @@
             </li>
             <li>
                 <a class="active-menu" href="/">
+                    <i class="fa fa-arrow-left fa-3x" aria-hidden="true"></i>
+                    Back to Site
+                </a>
+            </li>
+            <li>
+                <a href="">
                     <i class="fa fa-tachometer fa-3x" aria-hidden="true"></i>
                     Dashboard
                 </a>
             </li>
             <li>
-                <a href="ui.html"><i class="fa fa-desktop fa-3x"></i> UI Elements</a>
-            </li>
-            <li>
-                <a href="tab-panel.html"><i class="fa fa-qrcode fa-3x"></i> Tabs & Panels</a>
-            </li>
-            <li>
-                <a href="chart.html"><i class="fa fa-bar-chart-o fa-3x"></i> Morris Charts</a>
-            </li>
-            <li>
-                <a href="table.html"><i class="fa fa-table fa-3x"></i> Table Examples</a>
-            </li>
-            <li>
-                <a href="form.html"><i class="fa fa-edit fa-3x"></i> Forms </a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-sitemap fa-3x"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                <a href="#">
+                    <i class="fa fa-newspaper-o fa-3x" aria-hidden="true"></i>
+                    News section
+                    <span class="fa arrow"></span>
+                </a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="#">Second Level Link</a>
+                        <a href="">Create news</a>
                     </li>
                     <li>
-                        <a href="#">Second Level Link</a>
-                    </li>
-                    <li>
-                        <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                        </ul>
+                        <a href="">View news table</a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="blank.html"><i class="fa fa-square-o fa-3x"></i> Blank Page</a>
+                <a href="#">
+                    <i class="fa fa-pencil-square-o fa-3x" aria-hidden="true"></i>
+                    Blog section
+                    <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="">Create blog</a>
+                    </li>
+                    <li>
+                        <a href="">View blog table</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-newspaper-o fa-3x" aria-hidden="true"></i>
+                    Climate section
+                    <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="">Create new effect</a>
+                    </li>
+                    <li>
+                        <a href="">View climate table</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user-plus fa-3x" aria-hidden="true"></i>
+                    Add admin
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                    View all users
+                </a>
             </li>
         </ul>
     </div>
