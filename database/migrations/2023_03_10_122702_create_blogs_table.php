40b5cbe06a88863fa5bf8faae166136dc6f41f04
andyjohn23 <andrewnick906@gmail.com>
2023-03-11 06:59:07 +0300

    connect to database and new migration for the blog model

--- database/migrations/2023_03_10_122702_create_blogs_table.php
+++ database/migrations/2023_03_10_122702_create_blogs_table.php
@@ -15,6 +15,9 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('imageUrl');
+            $table->text('description');
             $table->timestamps();
         });
     }
