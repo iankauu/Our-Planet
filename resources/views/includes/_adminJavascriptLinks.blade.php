b083b3655e03a4b0116e4963ce91d32ef9f7b562
andyjohn23 <andrewnick906@gmail.com>
2023-03-11 14:12:40 +0300

    create new partial admin javascript link

---
+++ resources/views/includes/_adminJavascriptLinks.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ url('js/admin/jquery-1.10.2.js') }}"></script>
+<script src="{{ url('js/admin/bootstrap.min.js') }}"></script>
+<script src="{{ url('js/admin/jquery.metisMenu.js') }}"></script>
+<script src="{{ url('js/admin/morris/raphael-2.1.0.min.js') }}"></script>
+<script src="{{ url('js/admin/morris/morris.js') }}"></script>
+<script src="{{ url('js/admin/custom.js') }}"></script>
