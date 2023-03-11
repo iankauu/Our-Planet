987c5a531b4eaca02236c5d05718668d4e6ec5ed
andyjohn23 <andrewnick906@gmail.com>
2023-03-11 08:11:14 +0300

    add web app loader

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\PagesController@getHome' );
-Route::resource('/news', 'App\Http\Controllers\NewsController' );
-
+Route::get('/', 'App\Http\Controllers\PagesController@getHome');
+Route::resource('/news', 'App\Http\Controllers\NewsController');
